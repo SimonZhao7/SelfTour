07a4215f040369ede9fb84ef2073feeedab2a2d6
Ryan <92900778+ryanokimoto@users.noreply.github.com>
2024-04-06 05:27:59 -0700

    tours update

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -2,29 +2,15 @@ import React from 'react';
 import "./ToursList.css";
 const ToursList = () => {
   return (
-    <div>
-      <h1>Here are some createed tours!</h1>
-      <div className="tours-list">
-        <div className="tour">
-          <h1>Tour 1</h1>
-          <p>This is the first tour</p>
-          <img src="https://media.istockphoto.com/id/1136437406/photo/san-francisco-skyline-with-oakland-bay-bridge-at-sunset-california-usa.jpg?s=612x612&w=0&k=20&c=JVBBZT2uquZbfY0njYHv8vkLfatoM4COJc-lX5QKYpE=" 
-          alt="ggbridge" />
-        </div>
-        <div className="tour">
-          <h1>Tour 2</h1>
-          <p>This is the second tour</p>
-          <img src="https://media.istockphoto.com/id/1136437406/photo/san-francisco-skyline-with-oakland-bay-bridge-at-sunset-california-usa.jpg?s=612x612&w=0&k=20&c=JVBBZT2uquZbfY0njYHv8vkLfatoM4COJc-lX5QKYpE=" 
-          alt="ggbridge" />
-        </div>
-        <div className="tour">
-          <h1>Tour 3</h1>
-          <p>This is the third tour</p>
-          <img src="https://media.istockphoto.com/id/1136437406/photo/san-francisco-skyline-with-oakland-bay-bridge-at-sunset-california-usa.jpg?s=612x612&w=0&k=20&c=JVBBZT2uquZbfY0njYHv8vkLfatoM4COJc-lX5QKYpE=" 
-          alt="ggbridge" />
-        </div>
+  <div className="tours-list">
+    <div className="tour-container">
+      <h1 className='tour-title'>Tour 1</h1>
+      <div className="tour-content">
+        <p>This is the first tour</p>
+        <img className='tour-image' src="https://media.istockphoto.com/id/1136437406/photo/san-francisco-skyline-with-oakland-bay-bridge-at-sunset-california-usa.jpg?s=612x612&w=0&k=20&c=JVBBZT2uquZbfY0njYHv8vkLfatoM4COJc-lX5QKYpE=" alt="ggbridge" />
       </div>
     </div>
+  </div>
   );
 };
 
