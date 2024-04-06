bdf5f50168f2248d14990422855f67264907b43b
Ryan <92900778+ryanokimoto@users.noreply.github.com>
2024-04-06 04:01:14 -0700

    firebase with group email

--- firebase.tsx
+++ firebase.tsx
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-bNOxtlSN7QMzu25_JpW-GOjVVQzg2co",
-  authDomain: "selftours-c96e6.firebaseapp.com",
-  projectId: "selftours-c96e6",
-  storageBucket: "selftours-c96e6.appspot.com",
-  messagingSenderId: "970101690135",
-  appId: "1:970101690135:web:a81dd9e86a3691778bf152",
-  measurementId: "G-0T8C03PH8H"
+  apiKey: "AIzaSyB9VMDfbfyK4dHFImzu9T99lWrLy8q4e6I",
+  authDomain: "selftours-ff298.firebaseapp.com",
+  projectId: "selftours-ff298",
+  storageBucket: "selftours-ff298.appspot.com",
+  messagingSenderId: "869484098724",
+  appId: "1:869484098724:web:a86da73eae853bc284a231",
+  measurementId: "G-HRBL7X7E2C"
 };
 
 // Initialize Firebase
