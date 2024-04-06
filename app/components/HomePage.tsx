dbf58a47f7a5e4d8dd75d2bb740f81c9b639fb9c
Ryan <92900778+ryanokimoto@users.noreply.github.com>
2024-04-06 03:09:12 -0700

    homepage update

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import GeneratePlaces from "./GeneratePlaces";
-import Itinerary from "./Itinerary"
-
+import Itinerary from "./Itinerary";
+import "./HomePage.css";
+import { FaHome } from "react-icons/fa";
+import { IoEarth } from "react-icons/io5";
+import { CiCirclePlus } from "react-icons/ci";
 interface HomePageProps {
     // Define your props here
 }
 
 const HomePage: React.FC<HomePageProps> = (props) => {
     return (
-        <div>
-            <h1>Welcome to the Homepage!</h1>
+        <div className='page'>
+            <button className='home-button'>
+                <FaHome className='icon home' />
+            </button>
+            <button className='itinerary-button'>
+                <CiCirclePlus className='icon itinerary' />
+            </button>
+            <button className='browse-button'>
+                <IoEarth className='browse icon' />
+            </button>
+            <h1>
+                <text className='home-text'>
+                    Let's Tour!
+                </text>
+            </h1>
             <div className="homepage-content">
-                <Itinerary />
+                <button className='start-button'><text className='start-text' style={{ fontFamily: 'Roboto Mono, monospace' }}>start</text></button>
             </div>
-            
         </div>
     );
 }
