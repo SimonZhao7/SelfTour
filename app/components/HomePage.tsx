a3d6b7ca7bb982f643be87a5798570089db3669e
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-04-06 04:38:54 -0700

    made icons on index page route to proper routes

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -1,35 +1,46 @@
 import React from 'react';
-import Itinerary from "./Itinerary";
-import "./HomePage.css";
-import { FaHome } from "react-icons/fa";
-import { IoEarth } from "react-icons/io5";
-import { CiCirclePlus } from "react-icons/ci";
+import Itinerary from './Itinerary';
+import './HomePage.css';
+import { FaHome } from 'react-icons/fa';
+import { IoEarth } from 'react-icons/io5';
+import { CiCirclePlus } from 'react-icons/ci';
+
 interface HomePageProps {
-    // Define your props here
+	// Define your props here
 }
 
 const HomePage: React.FC<HomePageProps> = (props) => {
-    return (
-        <div className='page'>
-            <button className='home-button'>
-                <FaHome className='icon home' />
-            </button>
-            <button className='itinerary-button'>
-                <CiCirclePlus className='icon itinerary' />
-            </button>
-            <button className='browse-button'>
-                <IoEarth className='browse icon' />
-            </button>
-            <h1>
-                <text className='home-text'>
-                    Let's Tour!
-                </text>
-            </h1>
-            <div className="homepage-content">
-                <button className='start-button'><text className='start-text' style={{ fontFamily: 'Roboto Mono, monospace' }}>start</text></button>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className='page'>
+			<a href='/' className='home-button'>
+				{' '}
+				{/* Use anchor tag with href attribute */}
+				<FaHome className='icon home' />
+			</a>
+			<a href='/tours' className='itinerary-button'>
+				{' '}
+				{/* Use anchor tag with href attribute */}
+				<CiCirclePlus className='icon itinerary' />
+			</a>
+			<a href='/create' className='browse-button'>
+				{' '}
+				{/* Use anchor tag with href attribute */}
+				<IoEarth className='browse icon' />
+			</a>
+			<h1>
+				<text className='home-text'>Let's Tour!</text>
+			</h1>
+			<div className='homepage-content'>
+				<button className='start-button'>
+					<text
+						className='start-text'
+						style={{ fontFamily: 'Roboto Mono, monospace' }}>
+						start
+					</text>
+				</button>
+			</div>
+		</div>
+	);
+};
 
 export default HomePage;
