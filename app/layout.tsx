73be66b167552ca0751233c2581c3a7076691d1a
Reva Mahto <reva.mahto@gmail.com>
2024-04-07 09:50:32 -0700

    cleaned tour styling and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { FaHome } from 'react-icons/fa';
 import { IoEarth } from 'react-icons/io5';
 import { CiCirclePlus } from 'react-icons/ci';
 import { Metadata } from 'next/types';
+import styles from "./style.module.css";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,25 +23,27 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<nav>
-					<ul>
-						
+				<nav className={styles.navBar}>
+					<ul >
+
 						{/* Add the anchor tags here */}
-						<li>
-							<a href='/' className='home-button'>
-								<h1>home</h1>
-							</a>
-						</li>
-            <li>
-							<a href='/tours' className='browse-button'>
-								<h1>tours</h1>
-							</a>
-						</li>
-						<li>
-							<a href='/create' className='itinerary-button'>
-								<h1>create</h1>
-							</a>
-						</li>
+						<div >
+							<li >
+								<a href='/' className='home-button'>
+									<h1>home</h1>
+								</a>
+							</li>
+							<li>
+								<a href='/tours' className='browse-button'>
+									<h1>tours</h1>
+								</a>
+							</li>
+							<li>
+								<a href='/create' className='itinerary-button'>
+									<h1>create</h1>
+								</a>
+							</li>
+						</div>
 					</ul>
 				</nav>
 				{children}
