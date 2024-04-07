bcdaaa44593909872e03c32d7d505aa7e29cf9a7
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2024-04-07 10:53:24 -0700

    Add start tour link

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getDoc, doc } from "firebase/firestore";
 import { db } from "@/firebase";
 import styles from "./style.module.css";
 import TourDetail from "@/components/TourDetail";
+import Link from "next/link";
 
 const TourDetailView = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
@@ -13,6 +14,9 @@ const TourDetailView = async ({ params }: { params: { id: string } }) => {
   return (
     <div className={styles.wrapper}>
       <TourDetail tour={tour} />
+      <Link href={`/view/${id}`}>
+        <button className={styles.startBtn}>Start Tour</button>
+      </Link>
     </div>
   );
 };
